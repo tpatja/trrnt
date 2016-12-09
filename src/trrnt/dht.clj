069ff6c09d18c66e8faa6b8cd66de7873fee8e02
Teemu Patja <tp@iki.fi>
2016-12-09 22:21:34 +0200

    DHT KRPC message encoding/decoding
    * functions for encoding and decoding some of DHT's KRPC protocol
      messages + tests

---
+++ src/trrnt/dht.clj
@@ -0,0 +1,62 @@
+(ns trrnt.dht
+  (:require [trrnt.bencode :as b]))
+
+
+(defn- encode-dht-message [transaction-id message-type data-map]
+  (b/encode (merge  {"t" transaction-id
+                     "y" message-type}
+                    data-map)))
+
+(defn- encode-dht-q [transaction-id query-type query-args-map]
+  (encode-dht-message transaction-id "q" (merge {"q" query-type}
+                                                {"a" query-args-map})))
+
+
+(defn encode-ping-q [transaction-id querying-node-id]
+  (encode-dht-query transaction-id
+                    "ping"
+                    {"id" querying-node-id}))
+
+(defn encode-find-node-q [transaction-id querying-node-id target-node-id]
+  (encode-dht-query transaction-id
+                    "find_node"
+                    {"id" querying-node-id
+                     "target" target-node-id}))
+
+(defn encode-get-peers-q [transaction-id querying-node-id infohash]
+    (encode-dht-query transaction-id
+                    "get_peers"
+                    {"id" querying-node-id
+                     "info_hash" infohash}))
+
+
+(defn- decode-message-data
+  [encoded]
+  (let [decoded (b/decode encoded)
+        response? (nil? (get decoded "a"))]
+    (if response?
+      (decoded "r")
+      (decoded "a"))))
+
+(defn decode-find-node
+  "Decode a map with node-id, target-node-id and nodes keys from find_node query or response"
+  [encoded]
+  (let [message-data (decode-message-data encoded)]
+    {:node-id (get message-data "id")
+     :target (get message-data "target")
+     :nodes (get message-data "nodes")}))
+
+(defn decode-ping
+  "Decode a map with node-id key from ping query or response"
+  [encoded]
+  (let [message-data (decode-message-data encoded)]
+    {:node-id (get message-data "id")}))
+
+(defn decode-error
+  "Decode a map with error-code and error-message from given
+  stream with bencoded error"
+  [encoded]
+  (let [decoded (b/decode encoded)
+        [error-code error-message] (decoded "e")]
+    {:error-code error-code
+     :error-message error-message}))
---
+++ test/trrnt/dht_test.clj
@@ -0,0 +1,31 @@
+(ns trrnt.dht-test
+  (:require  [clojure.test :refer :all]
+             [clojure.java.io :as io]
+             [trrnt.dht :as sut]))
+
+
+(deftest decode-error-test
+  (let [encoded (.getBytes "d1:eli201e24:A Generic Error Occurrede1:t2:aa1:y1:ee")
+        decoded (sut/decode-error (io/input-stream encoded))]
+    (is (= (:error-code decoded) 201))
+    (is (= (:error-message decoded) "A Generic Error Occurred"))))
+
+
+(deftest ping-query-test
+  (let [transaction-id "asdgadsgdghh"
+        querying-node-id "asdgasdg12345"
+        encoded (sut/encode-ping-q transaction-id querying-node-id)
+        decoded  (sut/decode-ping (io/input-stream encoded))]
+    (is (= (:node-id decoded) querying-node-id))))
+
+(deftest find-node-test
+  (let [transaction-id "asdgadsgdghh"
+        querying-node-id "asdgasdg12345"
+        target-node-id "asdgasdg54321"
+        encoded (sut/encode-find-node-q transaction-id
+                                        querying-node-id
+                                        target-node-id)
+        decoded  (sut/decode-find-node (io/input-stream encoded))]
+    (is (= (:node-id decoded) querying-node-id))
+    (is (= (:target decoded) target-node-id))
+    (is (nil? (:nodes decoded)))))
